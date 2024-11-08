9f1157aad113291340c21efe9966ae287e2733b7
MedNewton <med.newton.2013@gmail.com>
2024-11-08 14:29:11 +0100

    Fixed hero size

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,4 @@
-import { Box, Container, Stack } from "@mui/material";
+import { Box, Stack } from "@mui/material";
 
 import HeroSection from "@sections/Hero";
 import AttractSpeakers from "@sections/SpeakersAttraction";
@@ -9,15 +9,13 @@ import TopSpeakers2024 from "@sections/TopSpeakers2024";
 export default function Home() {
   return (
     <Box width={'100vw'} display={'flex'} flexDirection={'column'} alignItems={'center'} justifyContent={'start'}>
-      <Container maxWidth={false} sx={{ padding: { xs: 1, sm: 1, md: 2.5 } }}>
-        <Stack width={'100%'} spacing={6}>
+        <Stack width={'100%'} spacing={6} padding={{ xs: 1, sm: 1, md: 2.5 }}>
           <HeroSection />
           <AttractSpeakers />
           <TopSponsors2024 />
           <Stats2024 />
           <TopSpeakers2024 />
         </Stack>
-      </Container>
     </Box>
   );
 }
--- src/app/sections/Hero.tsx
+++ src/app/sections/Hero.tsx
@@ -5,7 +5,7 @@ import Navbar from "@components/Navbar";
 
 const HeroSection = () => {
     return(
-        <Stack bgcolor={theme.palette.ne_lightblue.main} width={'100%'} height={'100vh'} padding={0} borderRadius={'1.4rem'} position={'relative'} overflow={'hidden'}>
+        <Stack width={'100%'} height={'95vh'} padding={0} borderRadius={'1.4rem'} position={'relative'} overflow={'hidden'}>
             <Stack sx={{
                 width: '100%',
                 height: 'auto',
