f32a32392bbe31d61dc14ed9fc9af19daf5496a1
MedNewton <med.newton.2013@gmail.com>
2025-02-13 02:00:05 +0100

    .

--- src/app/archive/2024/page.tsx
+++ src/app/archive/2024/page.tsx
@@ -12,7 +12,7 @@ export default function Archive2024() {
     <Box width={'100vw'} height={'100vh'} display={'flex'} flexDirection={'column'} alignItems={'center'} justifyContent={'center'} gap={2} sx={{
       overflow: 'hidden'
     }}>
-      <iframe src="https://napuleth.github.io/" width="100%" height="100%"></iframe>
+      <iframe src="https://napuleth2024.vercel.app/" width="100%" height="100%"></iframe>
 
     </Box>
   );
