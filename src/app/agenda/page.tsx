555f8d84d03fcb0903357363bfe2b21dd1e0e9a8
MedNewton <med.newton.2013@gmail.com>
2025-06-02 04:30:21 +0000

    .

--- src/app/agenda/page.tsx
+++ src/app/agenda/page.tsx
@@ -62,41 +62,7 @@ const Agenda = () => {
 
 
   return (
-    <>
-    <Box width={'100vw'} height={'100vh'} display={'flex'} flexDirection={'column'} alignItems={'center'} justifyContent={'center'} gap={2} sx={{
-      overflow: 'hidden'
-    }}>
-      <Stack width={'100%'} alignItems={'center'} justifyContent={'center'} gap={1}>
-        <Stack sx={{
-          animation: 'float 3s ease-in-out infinite', // Apply the animation
-          '@keyframes float': {
-            '0%': {
-              transform: 'translateY(0)', // Starting position
-            },
-            '50%': {
-              transform: 'translateY(-10px)', // Move the image up
-            },
-            '100%': {
-              transform: 'translateY(0)', // Move the image back down
-            },
-          },
-        }}>
-          <Image src={soli} alt="soli coming soon napuleth biggest web3 event southern italy 2025" height={300} width={300} ></Image>
-        </Stack>
-        <Typography width={'100%'} textAlign={'center'} variant="h4">Coming Soon...</Typography>
-        <Typography width={'100%'} textAlign={'center'} variant="h6">Agenda will be open very soon.</Typography>
-      </Stack>
-      <Link href="/" underline="none">
-        <Stack direction={'row'} alignItems={'center'} justifyContent={'center'} paddingY={2} paddingX={3} borderRadius={'10rem'} gap={1} sx={{
-          backgroundColor: theme.palette.ne_rose.main
-        }}>
-          <KeyboardBackspaceIcon fontSize="large" />
-          <Typography width={'100%'} textAlign={'center'} variant="h5">Back to Homepage</Typography>
-        </Stack>
-      </Link>
-
-    </Box>
-    <Stack display="none" width={'100%'} height={'100%'} alignItems={'start'} justifyContent={'center'}>
+    <Stack width={'100%'} height={'100%'} alignItems={'start'} justifyContent={'center'}>
       <Stack width={'100%'} height={'100%'} alignItems={'start'} justifyContent={'center'}>
         <Navbar isHome={false} />
       </Stack>
@@ -469,9 +435,7 @@ const Agenda = () => {
       <Stack width={'100%'} height={'100%'}>
         <Footer />
       </Stack>
-    </Stack>
-    </>
-    
+    </Stack>    
   );
 }
 
