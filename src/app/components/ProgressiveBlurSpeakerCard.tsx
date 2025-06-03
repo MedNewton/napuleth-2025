840137450aee024c93385afe7c7e40c2a2f8e0d9
MedNewton <med.newton.2013@gmail.com>
2025-06-03 00:18:59 +0000

    .

--- src/app/components/ProgressiveBlurSpeakerCard.tsx
+++ src/app/components/ProgressiveBlurSpeakerCard.tsx
@@ -49,7 +49,7 @@ const ProgressiveBlurSpeakerCard = ({ speaker }: ProgressiveBlurSpeakerCardProps
             }}>
                 <MdOutlineArrowOutward size={24} color={theme.palette.grey[800]} />
             </Box>
-            <Box position={'absolute'} bottom={0} left={0} width={'100%'} height={'55%'} className="gradient-blur">
+            <Box position={'absolute'} bottom={0} left={0} width={'100%'} height={'55%'} className="gradient-blur" overflow={'hidden'}>
                 <Box />
                 <Box />
                 <Box />
