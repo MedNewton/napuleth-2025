57657e4eaaa6734237a18390a22f2dff80342844
MedNewton <med.newton.2013@gmail.com>
2025-06-04 19:57:19 +0000

    .

--- src/app/sections/Bento.tsx
+++ src/app/sections/Bento.tsx
@@ -14,12 +14,11 @@ import vision from '@assets/window.png'
 
 const BentoSection = () => {
     return (
-        <Stack width={'100%'} alignItems={'center'} justifyContent={'center'} paddingTop={{ xs: 0, lg: 2 }} paddingBottom={{ xs: 0, lg: 2 }} paddingX={0}  marginTop={-2}>
+        <Stack width={'100%'} alignItems={'center'} justifyContent={'center'} paddingTop={{ xs: 0, lg: 2 }} paddingBottom={{ xs: 0, lg: 2 }} paddingX={0} marginTop={-2}>
             <Stack width={'100%'} direction={{ xs: 'column', lg: 'row' }} alignItems="stretch" justifyContent={'center'} gap={2}>
                 <Stack width={{ xs: '100%', lg: '50%' }}>
                     <CustomCard
-                        className="grain-background"
-                        gradient={`linear-gradient(180deg, rgba(237,212,254,1) 0%, rgba(237,212,254,0.5) 100%)`}
+                        gradient={`linear-gradient(180deg, #F9D4FA 0%, #ecbbed 100%)`}
                         shadow="0px 1px 3px rgba(0, 0, 0, 0.35)"
                         padding={0}
                         border={`none`}>
@@ -27,23 +26,12 @@ const BentoSection = () => {
                             <Stack gap={1} width={'100%'} position={'relative'} zIndex={2}>
                                 <Typography variant="h4" sx={{
                                     width: '75%'
-                                }}>Apply for NapulETH 2025 Hackathon</Typography>
-                                <Typography display={{ xs: 'none', lg: 'block' }} variant="h6" sx={{
-                                    width: '95%',
-                                    fontWeight: 400
-                                }}>
-                                    Participate in the 48H hackathon of NapulETH 2025, with an attractive prize pool and numerous bounties !<br />
-                                    TAIKAI is a hackathon platform that connects companies with talented developers to create solutions. We offer innovators a fun and exciting way to learn new skills, build cool projects, and earn rewards.<br />
-                                </Typography>
-                                <Typography variant="h6" display={{ xs: 'block', lg: 'none' }} sx={{
-                                    width: '95%',
-                                    fontWeight: 400
-                                }}>
-                                    Participate in the 48H hackathon of NapulETH 2025, with an attractive prize pool and numerous bounties !<br />
-                                    TAIKAI is a web3-based open innovation and hackathon platform that enables organizations to host challenges, competitions, and hackathons to crowdsource solutions from developers, designers, and entrepreneurs.<br />
+                                }}>Napuleth Visions</Typography>
+                                <Typography variant="h6" fontWeight={400} width={{ xs: '90%', lg: '90%' }}>
+                                    The exhibition Napuleth visions, through key concepts such as hybridization, concrescence and crystallization, explores how the process of “dematerialization” of value relates to the production of meaning in art, showing how the aesthetic and economic dimensions are constantly evolving, shaped by a network of interconnected actors.
                                 </Typography>
                             </Stack>
-                            <Link href="https://taikai.network/hackathons" underline="none" target="_blank">
+                            <Link href="https://napuleth-visions.vercel.app/" underline="none" target="_blank">
                                 <Button variant='outlined' sx={{
                                     background: theme.palette.info.main,
                                     fontWeight: 700,
@@ -55,7 +43,7 @@ const BentoSection = () => {
                                     }
                                 }}>
                                     <Typography variant="h6" paddingTop={0.5} fontWeight={700}>
-                                        Apply Now On TAIKAI
+                                        More about the exhibition
                                     </Typography>
                                 </Button>
                             </Link>
@@ -87,7 +75,7 @@ const BentoSection = () => {
                 <Stack width={{ xs: '100%', lg: '50%' }} direction={'column'} gap={2}>
                     <Stack width={'100%'} height={'50%'}>
                         <CustomCard
-                            gradient={`linear-gradient(180deg, #AEE8FE 0%, #CEF1FF 100%)`}
+                            gradient={`radial-gradient(112% 150% at 0% 100%, rgb(204, 43, 94) 0%, rgb(117, 58, 136) 100%)`}
                             shadow="0px 1px 3px rgba(0, 0, 0, 0.35)"
                             padding={0}
                             border={`none`}>
@@ -95,12 +83,12 @@ const BentoSection = () => {
                                 <Stack gap={1} width={'100%'} position={'relative'} zIndex={2}>
                                     <Typography variant="h4" sx={{
                                         width: '75%'
-                                    }}>Napuleth x Federico II</Typography>
+                                    }}>URBETH campus</Typography>
                                     <Typography variant="h6" fontWeight={400} width={{ xs: '90%', lg: '80%' }}>
-                                        Napuleth joined the University of Naples Federico II for Blockchain Day, highlighting our shared focus on innovation and real-world blockchain impact.
+                                        Urbe Campus – NapulETH edition is a 5-day bootcamp by urbe.eth, aimed at beginner developers looking to get started in Web3. It covers the basics of Solidity, Hardhat, and frontend interactions.
                                     </Typography>
                                 </Stack>
-                                <Link href="https://www.youtube.com/watch?v=UKoOHo5lVEI&t=640s" underline="none" target="_blank">
+                                <Link href="https://lu.ma/uf32lfsw" underline="none" target="_blank">
                                     <Button variant='outlined' sx={{
                                         background: theme.palette.background.default,
                                         fontWeight: 700,
@@ -112,16 +100,16 @@ const BentoSection = () => {
                                         }
                                     }}>
                                         <Typography variant="h6" paddingTop={0.5} fontWeight={700}>
-                                            Find Out More
+                                            More about the campus
                                         </Typography>
                                     </Button>
                                 </Link>
-                                <Image src={coin} alt="chainlink CCIP Nex Labs NexLabs Build Program" style={{
+                                <Image src={urbe} alt="chainlink CCIP Nex Labs NexLabs Build Program" style={{
                                     position: 'absolute',
-                                    height: '12rem',
+                                    height: '15rem',
                                     width: 'auto',
-                                    bottom: '-3rem',
-                                    right: '-3rem',
+                                    bottom: '-5rem',
+                                    right: '-5rem',
                                     zIndex: 1
                                 }} />
                             </Stack>
@@ -174,24 +162,30 @@ const BentoSection = () => {
                 </Stack>
             </Stack>
             <Stack width={'100%'} direction={{ xs: 'column', lg: 'row' }} alignItems="stretch" justifyContent={'center'} gap={2} marginTop={2}>
-                <Stack width={{ xs: '100%', lg: '60%' }}>
+                <Stack width={{ xs: '100%', lg: '100%' }}>
                     <CustomCard
-                        gradient={`radial-gradient(112% 150% at 0% 100%, rgb(204, 43, 94) 0%, rgb(117, 58, 136) 100%)`}
+                        className="grain-background"
+                        gradient={`linear-gradient(180deg, rgba(237,212,254,1) 0%, rgba(237,212,254,0.5) 100%)`}
                         shadow="0px 1px 3px rgba(0, 0, 0, 0.35)"
                         padding={0}
                         border={`none`}>
                         <Stack height={'100%'} width={'100%'} direction={'column'} alignItems={'start'} justifyContent={'space-between'} position={'relative'} gap={3} zIndex={3}>
                             <Stack gap={1} width={'100%'} position={'relative'} zIndex={2}>
-                                <Typography variant="h4" sx={{
-                                    width: '75%'
-                                }}>URBETH campus</Typography>
-                                <Typography variant="h6" fontWeight={400} width={{ xs: '90%', lg: '80%' }}>
-                                    Urbe Campus - NapulETH edition is an initiative by urbe.eth, dedicated to beginner developers, who will have the opportunity to learn new development skills in the Web3-Blockchain world.
-                                    <br />
-                                    This event offers a 5-day bootcamp covering the basics of Solidity development, Hardhat setup, and frontend interactions.
-                                </Typography>
+                                <Stack gap={1} width={'100%'} position={'relative'} zIndex={2}>
+                                    <Typography variant="h4" sx={{
+                                        width: '75%'
+                                    }}>Apply for NapulETH 2025 Hackathon</Typography>
+                                    <Typography variant="h6" sx={{
+                                        width: '60%',
+                                        fontWeight: 400
+                                    }}>
+                                        Participate in the 48H hackathon of NapulETH 2025, with an attractive prize pool and numerous bounties !<br />
+                                        TAIKAI is a hackathon platform that connects companies with talented developers to create solutions. We offer innovators a fun and exciting way to learn new skills, build cool projects, and earn rewards.<br />
+                                    </Typography>
+
+                                </Stack>
                             </Stack>
-                            <Link href="https://lu.ma/uf32lfsw" underline="none" target="_blank">
+                            <Link href="https://taikai.network/hackathons" underline="none" target="_blank">
                                 <Button variant='outlined' sx={{
                                     background: theme.palette.background.default,
                                     fontWeight: 700,
@@ -203,52 +197,32 @@ const BentoSection = () => {
                                     }
                                 }}>
                                     <Typography variant="h6" paddingTop={0.5} fontWeight={700}>
-                                        Find Out More
+                                        Apply Now On TAIKAI
                                     </Typography>
                                 </Button>
                             </Link>
-                            <Image src={urbe} alt="chainlink CCIP Nex Labs NexLabs Build Program" style={{
-                                position: 'absolute',
-                                height: '18rem',
-                                width: 'auto',
-                                bottom: '-5rem',
-                                right: '-5rem',
-                                zIndex: 1
-                            }} />
-                        </Stack>
-                    </CustomCard>
-                </Stack>
-                <Stack width={{ xs: '100%', lg: '40%' }}>
-                    <CustomCard
-                        gradient={`linear-gradient(180deg, #F9D4FA 0%, #F9D4FA 100%)`}
-                        shadow="0px 1px 3px rgba(0, 0, 0, 0.35)"
-                        padding={0}
-                        border={`none`}>
-                        <Stack height={'100%'} width={'100%'} direction={'column'} alignItems={'start'} justifyContent={'space-between'} position={'relative'} gap={3} zIndex={3}>
-                            <Stack gap={1} width={'100%'} position={'relative'} zIndex={2}>
-                                <Typography variant="h4" sx={{
-                                    width: '75%'
-                                }}>Napuleth Visions</Typography>
-                                <Typography variant="h6" fontWeight={400} width={{ xs: '90%', lg: '80%' }}>
-                                The exhibition Napuleth visions, through key concepts such as hybridization, concrescence and crystallization, explores how the process of “dematerialization” of value relates to the production of meaning in art, showing how the aesthetic and economic dimensions are constantly evolving, shaped by a network of interconnected actors.
-                                </Typography>
+                            <Stack display={{ xs: 'none', lg: 'block' }}>
+                                <Image src={triangle} alt="chainlink CCIP Nex Labs NexLabs Build Program" style={{
+                                    position: 'absolute',
+                                    height: '30rem',
+                                    width: 'auto',
+                                    top: '50%',
+                                    transform: 'translateY(-48%)',
+                                    right: '-7rem',
+                                    marginLeft: 'auto',
+                                    marginRight: 'auto',
+                                    zIndex: 1
+                                }} />
+                            </Stack><Stack display={{ xs: 'block', lg: 'none' }}>
+                                <Image src={triangle} alt="chainlink CCIP Nex Labs NexLabs Build Program" style={{
+                                    position: 'absolute',
+                                    height: '15rem',
+                                    width: 'auto',
+                                    bottom: '-5rem',
+                                    right: '-4.5rem',
+                                    zIndex: 1
+                                }} />
                             </Stack>
-                            <Link href="https://napuleth-visions.vercel.app/" underline="none" target="_blank">
-                                <Button variant='outlined' sx={{
-                                    background: theme.palette.background.default,
-                                    fontWeight: 700,
-                                    position: 'relative',
-                                    zIndex: 2,
-                                    '&:hover': {
-                                        backgroundColor: "black",
-                                        color: theme.palette.background.default
-                                    }
-                                }}>
-                                    <Typography variant="h6" paddingTop={0.5} fontWeight={700}>
-                                        Find Out More
-                                    </Typography>
-                                </Button>
-                            </Link>
                         </Stack>
                     </CustomCard>
                 </Stack>
