92230ad5a3de52fd6375d0bbb857fb33038f6534
MOHAMED BEN MOUSSA <MedNewton@users.noreply.github.com>
2025-06-05 08:55:17 +0100

    Update page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -26,6 +26,7 @@ export default function Home() {
         <Stack width={'100%'} spacing={{ xs: 4, lg: 4 }}>
           <Stack gap={{ xs: 2, lg: 0 }}>
             <Bento />
+            <AttractSpeakers />
           </Stack>
           <Stats2024 />
           <Speakers />
