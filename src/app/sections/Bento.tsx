87baa6230c9021aecb598299857882e08e5a35d7
MedNewton <med.newton.2013@gmail.com>
2025-06-17 13:50:30 +0100

    .

--- src/app/sections/Bento.tsx
+++ src/app/sections/Bento.tsx
@@ -15,6 +15,72 @@ import vision from '@assets/tyrian.webp'
 const BentoSection = () => {
     return (
         <Stack width={'100%'} alignItems={'center'} justifyContent={'center'} paddingTop={{ xs: 0, lg: 2 }} paddingBottom={{ xs: 0, lg: 2 }} paddingX={0} marginTop={-2}>
+           <Stack width={'100%'} direction={{ xs: 'column', lg: 'row' }} alignItems="stretch" justifyContent={'center'} gap={2} marginY={2}>
+                <Stack width={{ xs: '100%', lg: '100%' }}>
+                    <CustomCard
+                        className="grain-background"
+                        gradient={`linear-gradient(180deg, rgba(174, 232, 254,1) 0%, rgba(174, 232, 254,0.5) 100%)`}
+                        shadow="0px 1px 3px rgba(0, 0, 0, 0.35)"
+                        padding={0}
+                        border={`none`}>
+                        <Stack height={'100%'} width={'100%'} direction={'column'} alignItems={'start'} justifyContent={'space-between'} position={'relative'} gap={3} zIndex={3}>
+                            <Stack gap={1} width={'100%'} position={'relative'} zIndex={2}>
+                                <Stack gap={1} width={'100%'} position={'relative'} zIndex={2}>
+                                    <Typography variant="h4" sx={{
+                                        width: '75%'
+                                    }}>Apply for NapulETH 2025 Hackathon</Typography>
+                                    <Typography variant="h6" sx={{
+                                        width: { xs: '100%', lg: '60%' },
+                                        fontWeight: 400
+                                    }}>
+                                        Participate in the 48H hackathon of NapulETH 2025, with an attractive prize pool and numerous bounties !<br />
+                                        TAIKAI is a hackathon platform that connects companies with talented developers to create solutions. We offer innovators a fun and exciting way to learn new skills, build cool projects, and earn rewards.<br />
+                                    </Typography>
+
+                                </Stack>
+                            </Stack>
+                            <Link href="https://form.jotform.com/250915505161350" underline="none" target="_blank">
+                                <Button variant='outlined' sx={{
+                                    background: theme.palette.background.default,
+                                    fontWeight: 700,
+                                    position: 'relative',
+                                    zIndex: 2,
+                                    '&:hover': {
+                                        backgroundColor: "black",
+                                        color: theme.palette.background.default
+                                    }
+                                }}>
+                                    <Typography variant="h6" paddingTop={0.5} fontWeight={700}>
+                                        Apply Now On TAIKAI
+                                    </Typography>
+                                </Button>
+                            </Link>
+                            <Stack display={{ xs: 'none', lg: 'block' }}>
+                                <Image src={triangle} alt="chainlink CCIP Nex Labs NexLabs Build Program" style={{
+                                    position: 'absolute',
+                                    height: '30rem',
+                                    width: 'auto',
+                                    top: '50%',
+                                    transform: 'translateY(-48%)',
+                                    right: '-7rem',
+                                    marginLeft: 'auto',
+                                    marginRight: 'auto',
+                                    zIndex: 1
+                                }} />
+                            </Stack><Stack display={{ xs: 'block', lg: 'none' }}>
+                                <Image src={triangle} alt="chainlink CCIP Nex Labs NexLabs Build Program" style={{
+                                    position: 'absolute',
+                                    height: '15rem',
+                                    width: 'auto',
+                                    bottom: '-6rem',
+                                    right: '-4.5rem',
+                                    zIndex: 1
+                                }} />
+                            </Stack>
+                        </Stack>
+                    </CustomCard>
+                </Stack>
+            </Stack>
             <Stack width={'100%'} direction={{ xs: 'column', lg: 'row' }} alignItems="stretch" justifyContent={'center'} gap={2}>
                 <Stack width={{ xs: '100%', lg: '50%' }}>
                     <CustomCard
@@ -126,9 +192,12 @@ const BentoSection = () => {
                                     <Typography variant="h4" sx={{
                                         width: '100%'
                                     }}>Leveraging Thrilld Labs</Typography>
-                                    <Typography variant="h6" fontWeight={400} width={{ xs: '90%', lg: '95%' }}>
+                                    <Typography variant="h6" fontWeight={400} width={{ xs: '90%', lg: '95%' }} sx={{display: {xs: 'none', lg: 'block'}}}>
                                         NapulETH is now using Thrilld Labs as its official networking app!<br />
                                         Thrilld is a fair app that lets Web3 professionals find each other to talk business in a streamlined manner, online or at industry events.
+                                    </Typography>
+                                    <Typography variant="h6" fontWeight={400} width={{ xs: '90%', lg: '95%' }} sx={{display: {xs: 'block', lg: 'none'}}}>
+                                        NapulETH is now using Thrilld Labs as its official networking app!<br />
                                     </Typography>
                                 </Stack>
                                 <Link href="https://www.thrilldlabs.io/" underline="none" target="_blank">
@@ -161,72 +230,7 @@ const BentoSection = () => {
                     </Stack>
                 </Stack>
             </Stack>
-            <Stack width={'100%'} direction={{ xs: 'column', lg: 'row' }} alignItems="stretch" justifyContent={'center'} gap={2} marginTop={2}>
-                <Stack width={{ xs: '100%', lg: '100%' }}>
-                    <CustomCard
-                        className="grain-background"
-                        gradient={`linear-gradient(180deg, rgba(174, 232, 254,1) 0%, rgba(174, 232, 254,0.5) 100%)`}
-                        shadow="0px 1px 3px rgba(0, 0, 0, 0.35)"
-                        padding={0}
-                        border={`none`}>
-                        <Stack height={'100%'} width={'100%'} direction={'column'} alignItems={'start'} justifyContent={'space-between'} position={'relative'} gap={3} zIndex={3}>
-                            <Stack gap={1} width={'100%'} position={'relative'} zIndex={2}>
-                                <Stack gap={1} width={'100%'} position={'relative'} zIndex={2}>
-                                    <Typography variant="h4" sx={{
-                                        width: '75%'
-                                    }}>Apply for NapulETH 2025 Hackathon</Typography>
-                                    <Typography variant="h6" sx={{
-                                        width: { xs: '100%', lg: '60%' },
-                                        fontWeight: 400
-                                    }}>
-                                        Participate in the 48H hackathon of NapulETH 2025, with an attractive prize pool and numerous bounties !<br />
-                                        TAIKAI is a hackathon platform that connects companies with talented developers to create solutions. We offer innovators a fun and exciting way to learn new skills, build cool projects, and earn rewards.<br />
-                                    </Typography>
-
-                                </Stack>
-                            </Stack>
-                            <Link href="https://form.jotform.com/250915505161350" underline="none" target="_blank">
-                                <Button variant='outlined' sx={{
-                                    background: theme.palette.background.default,
-                                    fontWeight: 700,
-                                    position: 'relative',
-                                    zIndex: 2,
-                                    '&:hover': {
-                                        backgroundColor: "black",
-                                        color: theme.palette.background.default
-                                    }
-                                }}>
-                                    <Typography variant="h6" paddingTop={0.5} fontWeight={700}>
-                                        Apply Now On TAIKAI
-                                    </Typography>
-                                </Button>
-                            </Link>
-                            <Stack display={{ xs: 'none', lg: 'block' }}>
-                                <Image src={triangle} alt="chainlink CCIP Nex Labs NexLabs Build Program" style={{
-                                    position: 'absolute',
-                                    height: '30rem',
-                                    width: 'auto',
-                                    top: '50%',
-                                    transform: 'translateY(-48%)',
-                                    right: '-7rem',
-                                    marginLeft: 'auto',
-                                    marginRight: 'auto',
-                                    zIndex: 1
-                                }} />
-                            </Stack><Stack display={{ xs: 'block', lg: 'none' }}>
-                                <Image src={triangle} alt="chainlink CCIP Nex Labs NexLabs Build Program" style={{
-                                    position: 'absolute',
-                                    height: '15rem',
-                                    width: 'auto',
-                                    bottom: '-6rem',
-                                    right: '-4.5rem',
-                                    zIndex: 1
-                                }} />
-                            </Stack>
-                        </Stack>
-                    </CustomCard>
-                </Stack>
-            </Stack>
+            
         </Stack>
     )
 }
