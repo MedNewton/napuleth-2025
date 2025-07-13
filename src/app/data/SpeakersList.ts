22f73ca05a14d62a4d42fa7c42747082563449d4
Fabrizio Romano Genovese <fabrizio.romano.genovese@gmail.com>
2025-07-13 12:44:41 +0200

    Agenda updated.

--- src/app/data/SpeakersList.ts
+++ src/app/data/SpeakersList.ts
@@ -15,6 +15,7 @@ import DayanaAleksandrova from '@assets/speakers/New/webp/DayanaAleksandrova.web
 import PieraDiStefano from '@assets/speakers/New/webp/PieraDiStefano.webp'
 import JakubDziadkowiec from '@assets/speakers/New/webp/JakubDziadkowiec.webp'
 import HaoWang from '@assets/speakers/New/webp/HaoWang.webp'
+    import XiangruMa from '@assets/speakers/New/webp/XiangruMa.webp'
 import AlessandroMazza from '@assets/speakers/New/webp/AlessandroMazza.webp'
 import MarcoTullioGiordano from '@assets/speakers/New/webp/MarcoTullioGiordano.webp'
 import CarloParisi from '@assets/speakers/New/webp/CarloParisi.webp'
@@ -349,6 +350,17 @@ export const speakersList: Speaker[] = [
         announced: true,
         id: 16,
     },
+    // {
+    //     name: "Xiangru Ma",
+    //     image: XiangruMa.src,
+    //     url: "",
+    //     appartenance: [{
+    //         name: "Crystality",
+    //         url: "",
+    //     }],
+    //     announced: true,
+    //     id: 17,
+    // },
     {
         name: "Hao Wang",
         image: HaoWang.src,
