15de0a428288d2dd6ee52d1c82cc6b915d5ed0a3
MedNewton <med.newton.2013@gmail.com>
2025-09-11 03:43:12 +0100

    .

---
+++ src/app/archive/2025/page.tsx
@@ -0,0 +1,14 @@
+import { Box } from "@mui/material";
+
+
+
+export default function Archive2024() {
+  return (
+    <Box width={'100vw'} height={'100vh'} display={'flex'} flexDirection={'column'} alignItems={'center'} justifyContent={'center'} gap={2} sx={{
+      overflow: 'hidden'
+    }}>
+      <iframe src="https://napuleth2025-6a26.vercel.app/" width="100%" height="100%"></iframe>
+
+    </Box>
+  );
+}
